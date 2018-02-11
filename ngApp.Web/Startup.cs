fd015594b115f44e79db8b6c7a6de2e741fc4f7b
marius didjurgis <marius.didjurgis@gmail.com>
2018-02-11 19:32:14 +0200

    error

--- ngApp.Web/Startup.cs
+++ ngApp.Web/Startup.cs
@@ -24,7 +24,7 @@ namespace ngApp.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<NgAppDbContext>(options => options.UseSqlServer(Configuration["ConnectionString:Default"]);
+            services.AddDbContext<NgAppDbContext>(options => options.UseSqlServer(Configuration["ConnectionString:Default"]));
             services.AddMvc();
         }
 
