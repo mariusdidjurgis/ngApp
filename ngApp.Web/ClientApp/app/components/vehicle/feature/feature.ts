2bbd4cbc206915738405ff94b92e89e2cf65ede6
marius didjurgis <marius.didjurgis@gmail.com>
2018-03-18 21:26:49 +0200

    description in feature

--- ngApp.Web/ClientApp/app/components/vehicle/feature/feature.ts
+++ ngApp.Web/ClientApp/app/components/vehicle/feature/feature.ts
@@ -1,7 +1,7 @@
 import { WithId } from "../../shared/interfces/withId.interface";
 
 export class Feature implements WithId {
-    constructor(public Id: number, public Name: string, public Code: string) {
+    constructor(public Id: number, public Name: string, public Code: string, public Description: string = '') {
 
     }
 }
--- ngApp.Web/ViewModels/Vehicles/FeatureViewModel.cs
+++ ngApp.Web/ViewModels/Vehicles/FeatureViewModel.cs
@@ -6,17 +6,20 @@ namespace ngApp.Web.ViewModels
     public class FeatureViewModel : BaseNameViewModel<Feature>
     {
         public string Code { get; set; }
+        public string Description { get; set; }
 
         public FeatureViewModel() { }
         public FeatureViewModel(Feature model) : base(model)
         {
             Code = model.Code;
+            Description = model.Description;
         }
 
         public override void GetUpdatedModel(Feature model)
         {
             base.GetUpdatedModel(model);
             model.Code = Code;
+            model.Description = Description;
         }
 
     }
