1137dfd4abd3b9fcfda6d89ed4c0b184546d9382
marius didjurgis <marius.didjurgis@gmail.com>
2018-10-28 15:29:25 +0200

    singleton object

---
+++ ngApp.Tests/ngAppWeb/DesignPatterns/Singleton/SingletonTest.cs
@@ -0,0 +1,54 @@
+﻿using ngApp.Web.Controllers;
+using ngApp.Web.DesignPatterns.Singleton;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ngApp.Tests.ngAppWeb.DesignPatterns
+{
+    public class SingletonTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void GetInstance_CallSingletonObjectsInDifferentThreads_ShouldNotReturnSameObject()
+        {
+            Singleton singleton1 = Singleton.GetInstance;
+            Singleton singleton2 = Singleton.GetInstance;            
+
+            Assert.AreEqual(Singleton.GetInstance, singleton1);
+            Assert.AreEqual(Singleton.GetInstance, singleton2);
+        }
+
+
+        [Test]
+        public void GetInstance_SimpleCallOnThreadSafe_ShouldReturnSameObject()
+        {
+            var singleton1 = Singleton2.GetInstance;
+            var singleton2 = Singleton2.GetInstance;
+
+            Assert.AreEqual(Singleton2.GetInstance, singleton1);
+            Assert.AreEqual(Singleton2.GetInstance, singleton2);
+            Assert.AreEqual(singleton1, singleton2);
+        }
+
+        [Test]
+        public void GetInstance_SimpleCallOnThreadSafe2_ShouldReturnSameObject()
+        {
+            var singleton1 = Singleton3.GetInstance;
+            var singleton2 = Singleton3.GetInstance;
+
+            Assert.AreEqual(Singleton3.GetInstance, singleton1);
+            Assert.AreEqual(Singleton3.GetInstance, singleton2);
+            Assert.AreEqual(singleton1, singleton2);
+        }
+
+    }
+}
---
+++ ngApp.Web/DesignPatterns/Singleton/Singleton.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ngApp.Web.DesignPatterns.Singleton
+{
+    public class SingletonObject
+    {
+        private static SingletonObject _instance = new SingletonObject(); //Thread not safe
+
+        public static SingletonObject GetInstance
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+
+        private SingletonObject() { }
+    }
+
+    public class SingletonObject2
+    {
+        private static readonly SingletonObject2 _instance = new SingletonObject2();//readonly threadSafe
+
+        public static SingletonObject2 GetInstance
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+
+        private SingletonObject2()
+        {
+
+        }
+
+    }
+
+    public class SingletonObject3
+    {
+        private static readonly object _lock = new object();
+        private static SingletonObject3 _instance;
+
+        public static SingletonObject3 GetInstance
+        {
+            get
+            {
+                lock (_lock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new SingletonObject3();
+                    }
+
+                    return _instance;
+                }
+            }
+        }
+
+        private SingletonObject3()
+        {
+
+        }
+
+    }
+
+}
